63535115d665d162a3652aaec6ab5d64bc230c5b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-05-03 11:55:00 -0700

    Extract taskweft-plans from taskweft-mcp

---
+++ mix.exs
@@ -0,0 +1,11 @@
+defmodule Taskweft.Plans.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :taskweft_plans,
+      version: "0.1.0",
+      elixir: "~> 1.17"
+    ]
+  end
+end
